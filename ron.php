67bf312fc450f749dd98c894c615a8d050fd5b31
Scott Paeth <scott.paeth@freshgrade.com>
2016-09-15 00:00:00 -0700

    initial working commit

---
+++ ron.php
@@ -0,0 +1,15 @@
+<?php
+
+$ronFacts = array(
+	"When Ron codes, he does it on top of a Jenga tower.",
+	"Ron's blood is now 82% beer.",
+	"Every time Ron commits code, a delicious candy disappears from somewhere in the world.",
+	":cat2: :gun: ",
+	":rj0::rj1::rj2::rj3:\n:rj4::rj5::rj6::rj7:\n:rj8::rj9::rj10::rj11:\n:rj12::rj13::rj14::rj15:"
+);
+
+$rand = array_rand($ronFacts);
+
+header('Content-Type: application/json');
+
+print('{ "response_type": "in_channel", "text": "' . $ronFacts[$rand] . '"}');
